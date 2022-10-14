58e67db89bff4a22be2fdf692d816c106e835599
unknown <dmendoza096@gmail.com>
2022-10-14 18:37:57 -0500

    Historias20_23

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import { FaStar } from "react-icons/fa";
+
+const colors = {
+    orange: "#FFBA5A",
+    grey: "#a9a9a9"
+
+};
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [currentValue, setCurrentValue] = useState(0);
+    const [hoverValue, setHoverValue] = useState(undefined);
+
+    const stars = Array(5).fill(0)
+    const handleClick = value => {
+        setCurrentValue(value)
+    }
+    const handleMouseOver = newHoverValue => {
+        setHoverValue(newHoverValue)
+    };
+    const handleMouseLeave = () => {
+        setHoverValue(undefined)
+    }
+
+
+    return (
+        <div style={styles.container}>
+            <h2> ¡GRACIAS PRO TU ORDEN! </h2>
+            <h1>CALIFICANOS</h1>
+            <div style={styles.stars}>
+                {stars.map((_, index) => {
+                    return (
+                        <FaStar
+                            key={index}
+                            size={24}
+                            onClick={() => handleClick(index + 1)}
+                            onMouseOver={() => handleMouseOver(index + 1)}
+                            onMouseLeave={handleMouseLeave}
+                            color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+                            style={{
+                                marginRight: 10,
+                                cursor: "pointer"
+                            }}
+                        />
+                    )
+                })}
+            </div>
+            <textarea
+                placeholder="Cuentanos tu experiencia"
+                style={styles.textarea}
+            />
+
+            <button
+                style={styles.button}
+            >
+                Submit
+            </button>
+
+        </div>
+    );
+};
+
+
+
+
+
+
 
 export default App;
